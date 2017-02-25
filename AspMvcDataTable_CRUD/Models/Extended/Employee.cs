ff4fa9fc6b83deb0fba24c6bdd41db84c8ad669e
bonbonpa <darksphin@gmail.com>
2017-02-25 13:45:56 +0700

    Create MetaData

---
+++ AspMvcDataTable_CRUD/Models/Extended/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AspMvcDataTable_CRUD.Models
+{
+    [MetadataType(typeof(EmployeeMetada))]
+    public partial class Employee
+    {
+
+    }
+
+    public class EmployeeMetada
+    {
+        [Required(AllowEmptyStrings = false , ErrorMessage = "Please provide first name")]
+        public string FirstName { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please provide last name")]
+        public string LastName { get; set; }
+
+    }
+}
